c46a0426cff04ff6809dc627301589d4ed20b467
John Ademoye <moyojohn188@gmail.com>
2019-12-04 23:26:19 +0100

    Add project files.

---
+++ CategoryTodoAPI/Models/MyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CategoryTodoAPI.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
